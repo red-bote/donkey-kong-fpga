93bb7862a4f6b3c79f75c63cf9fb054695fab737
Alex <d18c7db@hotmail.com>
2023-03-27 20:50:40 +0200

    License, Readme and credits added

--- source/dkong/dkong_rams.vhd
+++ source/dkong/dkong_rams.vhd
@@ -1,5 +1,7 @@
 --	(c) 2012 d18c7db(a)hotmail
 --
+-- Originally by Katsumi Degawa in Verilog
+--
 --	This program is free software; you can redistribute it and/or modify it under
 --	the terms of the GNU General Public License version 3 or, at your option,
 --	any later version as published by the Free Software Foundation.
